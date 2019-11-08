a2b2994554b011cbe899bd56f3ff3b3391bbaa93
pranjal darekar <pranjaldarekar10@gmail.com>
2019-11-08 13:13:14 +0530

    Add files via upload

---
+++ Assignment1_2.py
@@ -0,0 +1,13 @@
+#program contains checknum function to check and print even or odd no
+def checksum():
+	print("Enter a number");
+	num = int(input());
+
+	if (num % 2) == 0:
+   		print("Given number is Even");
+	else:
+  		print("Given number is Odd");
+
+
+checksum();
+
