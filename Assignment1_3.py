660da030b8fb8aedaeb328c11182b70910a74de6
pranjal darekar <pranjaldarekar10@gmail.com>
2019-11-09 01:20:33 +0530

    Add files via upload

---
+++ Assignment1_3.py
@@ -0,0 +1,11 @@
+#program to add two numbers
+def add():
+
+	num1 = int(input("Enter First number"));12
+
+	num2 = int(input("Enter Second number"));
+
+	iRet = num1 + num2;
+	print("Addition is",iRet);
+
+add();
